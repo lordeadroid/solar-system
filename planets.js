b216b25fc9747f3c8b68414d08d8cd2d6de03a1d
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 19:31:49 +0530

    decreased planet revolution speed

--- planets.js
+++ planets.js
@@ -1,7 +1,7 @@
 const planet = ({ distance, speed, color, orbitalTilt }) => {
   push();
   fill(color);
-  rotateY((millis() / 50000) * speed);
+  rotateY((millis() / 100000) * speed);
   rotateZ(radians(orbitalTilt));
   translate(distance * 120, 0, 0);
   sphere(30);
