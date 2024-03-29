8b947b7263716360f6ae913c12e02450966601b2
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 14:56:47 +0530

    reduced size of planets

--- planets.js
+++ planets.js
@@ -4,6 +4,6 @@ const planet = ({ distance, speed, color, orbitalTilt }) => {
   rotateY((millis() / 50000) * speed);
   rotateZ(radians(orbitalTilt));
   translate(distance * 120, 0, 0);
-  sphere(40);
+  sphere(30);
   pop();
 };
