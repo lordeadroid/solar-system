4dbda14fa86334d5d92680533e392b35bfa90be2
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 13:56:40 +0530

    changed method to calculate rotation for planets

--- planets.js
+++ planets.js
@@ -1,7 +1,7 @@
 const planet = ({ distance, speed, color }) => {
   push();
   fill(color);
-  rotateY(frameCount * 0.01);
+  rotateY((frameCount / 4000) * speed);
   translate(distance * 120, 0, 0);
   sphere(40);
   pop();
