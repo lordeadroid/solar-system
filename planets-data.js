55ff5c1af6c4a535cc60317e9ed1d3b3b8fb6568
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 13:40:21 +0530

    added planets data

---
+++ planets-data.js
@@ -0,0 +1,42 @@
+const planetsData = {
+  mercury: {
+    distance: 1,
+    speed: 365,
+    color: "gray",
+  },
+  venus: {
+    distance: 1.8,
+    speed: 248,
+    color: "orange",
+  },
+  earth: {
+    distance: 2.5,
+    speed: 137,
+    color: "blue",
+  },
+  mars: {
+    distance: 3.9,
+    speed: 21,
+    color: "rust",
+  },
+  jupiter: {
+    distance: 5.2,
+    speed: 8.5,
+    color: "brown",
+  },
+  saturn: {
+    distance: 24,
+    speed: 2.9,
+    color: "skyblue",
+  },
+  uranus: {
+    distance: 50,
+    speed: 1.5,
+    color: "darkblue",
+  },
+  neptune: {
+    distance: 77.4,
+    speed: 1,
+    color: "violet",
+  },
+};
