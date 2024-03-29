fcc4987041f41b14eb3df9bc62bc0f0f44f4e7ae
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 14:49:36 +0530

    added orbital tilt for planets

--- planets-data.js
+++ planets-data.js
@@ -3,40 +3,48 @@ const planetsData = {
     distance: 1,
     speed: 365,
     color: "gray",
+    orbitalTilt: 6.3,
   },
   venus: {
     distance: 1.8,
     speed: 248,
     color: "orange",
+    orbitalTilt: 2.2,
   },
   earth: {
     distance: 2.5,
     speed: 137,
     color: "blue",
+    orbitalTilt: 1.6,
   },
   mars: {
     distance: 3.9,
     speed: 21,
     color: "rust",
+    orbitalTilt: 1.7,
   },
   jupiter: {
     distance: 5.2,
     speed: 8.5,
     color: "brown",
+    orbitalTilt: 0.3,
   },
   saturn: {
     distance: 24,
     speed: 2.9,
     color: "skyblue",
+    orbitalTilt: 0.9,
   },
   uranus: {
     distance: 50,
     speed: 1.5,
     color: "darkblue",
+    orbitalTilt: 1,
   },
   neptune: {
     distance: 77.4,
     speed: 1,
     color: "violet",
+    orbitalTilt: 0.7,
   },
 };
--- planets.js
+++ planets.js
@@ -1,7 +1,8 @@
-const planet = ({ distance, speed, color }) => {
+const planet = ({ distance, speed, color, orbitalTilt }) => {
   push();
   fill(color);
   rotateY((millis() / 50000) * speed);
+  rotateZ(radians(orbitalTilt));
   translate(distance * 120, 0, 0);
   sphere(40);
   pop();
