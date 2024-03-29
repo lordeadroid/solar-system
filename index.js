90babfe1bd32a33d25d4cc17298166110d0c0584
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 13:18:13 +0530

    added revolution to planets

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const sun = () => {
 const planet = (color, distanceFromSun) => {
   push();
   fill(color);
+  rotateY(frameCount * 0.01);
   translate(distanceFromSun, 0, 0);
   sphere(50);
   pop();
