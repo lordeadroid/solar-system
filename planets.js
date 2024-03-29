d808a3aa00c4070a814e3b7c43b4a91230708531
lordeadroid <rishabhrawat34@gmail.com>
2024-03-29 14:34:45 +0530

    calculating revolution using millis instead of frameCount

--- planets.js
+++ planets.js
@@ -1,7 +1,7 @@
 const planet = ({ distance, speed, color }) => {
   push();
   fill(color);
-  rotateY((frameCount / 4000) * speed);
+  rotateY((millis() / 50000) * speed);
   translate(distance * 120, 0, 0);
   sphere(40);
   pop();
